97b8e8ac9c2a70cc9e060049c4099ac88531f717
Peanut-juice <ajonathan551404@gmail.com>
2026-05-21 16:23:30 -0400

    Added methods and instance variables

--- Assets/Codes/Element.cs
+++ Assets/Codes/Element.cs
@@ -1,4 +1,27 @@
-public abstract class Zombie : MonoBehaviour {
-    public float speed, health, damage;
-    void Update() { MoveLeft(); CheckForPlant(); }
+using UnityEngine;
+public abstract class Element : MonoBehaviour {
+    public float speed, health, damage, drop;
+    public float X { get; private set; }
+    public float Y { get; private set; }
+    protected virtual void Awake() {
+        X = transform.position.x;
+        Y = transform.position.y;
+    }
+    public abstract void moveLeft();
+    public bool checkForPlant(Compound C) {
+        if (X == C.X && Y == C.Y) {
+            C.isEaten(damage);
+            return true;
+        }
+        return false;
+    }
+    public void isDefeated(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    void Update() { 
+        moveLeft();
+    }
 }
