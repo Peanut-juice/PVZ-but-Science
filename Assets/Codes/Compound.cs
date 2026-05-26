126cbfb39b43ea6e9175f1f0f4d9bc3f74a58f6d
Peanut-juice <ajonathan551404@gmail.com>
2026-05-26 18:32:50 -0400

    Spacing out

--- Assets/Codes/Compound.cs
+++ Assets/Codes/Compound.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 public abstract class Compound : MonoBehaviour {
     public int cost;
     public float attackCooldown, health, placementCooldown, timer;
-    public float X { get; private set; }
-    public float Y { get; private set; }
+    public float X { 
+        get; 
+        private set; 
+    }
+    public float Y { 
+        get; 
+        private set; 
+    }
     protected virtual void Awake() {
         X = transform.position.x;
         Y = transform.position.y;
